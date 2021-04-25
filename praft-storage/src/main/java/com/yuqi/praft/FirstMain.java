a7e4313d4d10aa7841ff4afd4d5de6f30a8826d2
yuqi1129 <yuqi4733@gmail.com>
2021-04-25 20:50:20 +0800

    init project

---
+++ praft-storage/src/main/java/com/yuqi/praft/FirstMain.java
@@ -0,0 +1,46 @@
+package com.yuqi.praft;
+
+import org.rocksdb.Options;
+import org.rocksdb.RocksDB;
+import org.rocksdb.RocksDBException;
+import org.rocksdb.RocksIterator;
+
+/**
+ * @author yuqi
+ * @mail yuqi5@xiaomi.com
+ * @description your description
+ * @time 25/4/21 下午8:04
+ **/
+public class FirstMain {
+    static {
+        RocksDB.loadLibrary();
+    }
+    public static void main(String[] args) {
+        final String storagePath = "/Users/yuqi/test/rocksdb";
+
+        Options options = new Options();
+        options.setCreateIfMissing(true);
+
+        try (RocksDB rocksDB = RocksDB.open(options,storagePath)){
+            for (int i = 0; i < 10000; i++) {
+                rocksDB.put(String.valueOf(i).getBytes(), String.valueOf(i * i).getBytes());
+            }
+        } catch (RocksDBException e) {
+            e.printStackTrace();
+        }
+
+
+        try (RocksDB rocksDB = RocksDB.open(options,storagePath)){
+            RocksIterator rocksIterator = rocksDB.newIterator();
+            rocksIterator.seekToFirst();
+
+            while(rocksIterator.isValid()) {
+                System.out.println("key = " + new String(rocksIterator.key()) + " , value = " + new String(rocksIterator.value()));
+                rocksIterator.next();
+            }
+        } catch (RocksDBException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
